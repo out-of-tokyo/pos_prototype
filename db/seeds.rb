f726d58a5115af04fccdafdca866c8516f315c41
daichi <daipanchi@gmail.com>
2014-09-09 18:30:10 +0900

    Create db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!([
+  {name: "サクサクしっとりチョコ", barcode_id: 4903326112852},
+  {name: "ぷりぷり鮭おにぎり", barcode_id: 4903326112853},
+])
+Store.create!([
+  {name: "ローソン西早稲田店", beacon_id: "D87CEE67-C2C2-44D2-A847-B728CF8BAAAD"},
+  {name: "ローソン東早稲田店", beacon_id: "96996D3D-E135-4330-99C8-3ABF9489FE53"},
+])
+ProductStore.create!([
+  {product_id: 1, store_id: 1, price: 108, stock: 1},
+  {product_id: 1, store_id: 2, price: 108, stock: 2},
+  {product_id: 2, store_id: 1, price: 208, stock: 3},
+  {product_id: 2, store_id: 2, price: 228, stock: 4},
+])
